815343aa85aca46963ffc2d1a9fe662824279e01
vivianhan404 <viv.han.12@gmail.com>
2025-03-21 14:53:31 -0400

    Add config, consent, debrief slides

--- edit_video.py
+++ edit_video.py
@@ -1,26 +1,9 @@
-import cv2
 from moviepy import VideoFileClip
-
-def crop_buttons(old_name, new_name, x, y, w, h):
-    cap = cv2.VideoCapture(old_name)
-    fps, num_frames = cap.get(cv2.CAP_PROP_FPS), cap.get(cv2.CAP_PROP_FRAME_COUNT)
-
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(new_name, fourcc, fps, (w, h))
-
-    for _ in range(int(num_frames - 8 * fps)):
-        ret, frame = cap.read()
-        assert(ret)
-        crop_frame = frame[y:y+h, x:x+w]
-        out.write(crop_frame)
-
-    cap.release()
-    out.release()
 
 def crop_video(old_name, new_name):
     clip = (
         VideoFileClip(old_name)
-        .subclipped(0, -8)
+        .subclipped(0, -3)
         .cropped(x1=0,y1=0,width=1920,height=470)
     )
     clip.write_videofile(new_name)
@@ -28,41 +11,42 @@ def crop_video(old_name, new_name):
 def crop_button_2(old_name, new_name, x1, y1, w, h):
     clip = (
         VideoFileClip(old_name)
-        .subclipped(0, -8)
+        .subclipped(0, -3)
         .with_volume_scaled(0)
         .cropped(x1=x1,y1=y1,width=w,height=h)
     )
     clip.write_videofile(new_name)
 
-trials = [
-    "EasyMonotonic_1",
-    "EasySymmetric_1",
-    "HardMonotonic_1",
-    "HardSymmetric_1",
-    "EasyMonotonic_2",
-    "EasySymmetric_2",
-    "HardMonotonic_2",
-    "HardSymmetric_2",
-]
+# trials = [
+#     "EasyMonotonic_1",
+#     "EasySymmetric_1",
+#     "HardMonotonic_1",
+#     "HardSymmetric_1",
+#     "EasyMonotonic_2",
+#     "EasySymmetric_2",
+#     "HardMonotonic_2",
+#     "HardSymmetric_2",
+# ]
 
 fps = 30
 left = (0,470,825,425)
 right = (1095,470,825,425)
 
-for idx, name in enumerate(trials):
-    old_n = int(3 * idx + 9)
-    new_n = int(2 * idx + 7)
-    crop_button_2(f'old_materials/mp4/Movie{old_n}_A_{name}_video.mp4', 
-                    f'mp4/Movie{new_n}_A_{name}_left.mp4', *left)
-    crop_button_2(f'old_materials/mp4/Movie{old_n}_A_{name}_video.mp4', 
-                    f'mp4/Movie{new_n}_A_{name}_right.mp4', *right)
+# for idx, name in enumerate(trials):
+#     old_n = int(3 * idx + 9)
+#     new_n = int(2 * idx + 7)
+#     crop_button_2(f'old_materials/mp4/Movie{old_n}_A_{name}_video.mp4', 
+#                     f'mp4/Movie{new_n}_A_{name}_left.mp4', *left)
+#     crop_button_2(f'old_materials/mp4/Movie{old_n}_A_{name}_video.mp4', 
+#                     f'mp4/Movie{new_n}_A_{name}_right.mp4', *right)
 
-    crop_button_2(f'old_materials/mp4/Movie{old_n}_B_{name}_video.mp4', 
-                    f'mp4/Movie{new_n}_B_{name}_left.mp4', *left)
-    crop_button_2(f'old_materials/mp4/Movie{old_n}_B_{name}_video.mp4', 
-                    f'mp4/Movie{new_n}_B_{name}_right.mp4', *right)
+#     crop_button_2(f'old_materials/mp4/Movie{old_n}_B_{name}_video.mp4', 
+#                     f'mp4/Movie{new_n}_B_{name}_left.mp4', *left)
+#     crop_button_2(f'old_materials/mp4/Movie{old_n}_B_{name}_video.mp4', 
+#                     f'mp4/Movie{new_n}_B_{name}_right.mp4', *right)
 
-# crop_button_2(f'old_materials/mp4/Movie6_Inclusion2_video.mp4', f'mp4/Movie5_Inclusion2_left.mp4', *left)
-# crop_button_2(f'old_materials/mp4/Movie6_Inclusion2_video.mp4', f'mp4/Movie5_Inclusion2_right.mp4', *right)
+crop_button_2('old_materials/mp4/Movie3_Inclusion1_video.mp4', 'mp4/Movie3_Inclusion1_left.mp4', *left)
+crop_button_2('old_materials/mp4/Movie3_Inclusion1_video.mp4', 'mp4/Movie3_Inclusion1_right.mp4', *right)
+crop_video('old_materials/mp4/Movie3_Inclusion1_video.mp4', 'mp4/Movie3_Inclusion1_top.mp4')
 
 
--- rmts_study_design_chs.js
+++ rmts_study_design_chs.js
@@ -50,26 +50,26 @@ function makeChoiceTrial(top_video, left_button, right_button) {
 
 //  ===== SETUP =============================================
 
-// const videoConfig = {
-//   type: chsRecord.VideoConfigPlugin,
-// };
-// timeline.push(videoConfig);
+const videoConfig = {
+  type: chsRecord.VideoConfigPlugin,
+};
+timeline.push(videoConfig);
 
-// const videoConsent = {
-//   type: chsRecord.VideoConsentPlugin,
-//   PIName: "Laura Schulz",
-//   institution: "Massachussetts Institute of Technology",
-//   PIContact: "Laura Schulz (contact: nhcoates@mit.edu)",
-//   payment:
-//     "You can only participate in this study once for compensation. After you finish the study, we will email you a $5 Amazon gift card from Amazon.com (via a code that can be entered online) within ten days to thank you for your time. To be eligible for the gift card, (1) your child must be in the age range for this study, (2) English is (one of) your child's first language(s), (3) need to submit a valid consent statement in which we can see you AND your child in the frame, (4) must not have participated in similar matching tasks (you will get a warning message if this is the case!). Please make sure you are with your child throughout, especially if they cannot click the buttons (they can point and you can click their answer).",
-//   procedures:
-//     "In this study, you and your child will see eight matching tasks that involve boxes with shapes. Each time, you and your child will listen to the instructions, and pick which box matches best with the target box. Throughout the experiment, you or your child will click to make responses (if your child cannot click, they can point to the answer and you can click the answer they choose).",
-//   purpose:
-//     "This study aims to explore how very young children, age 4 to 5-years-old, come to understand relational mappings. This is worth exploring because many studies suggest that young children are more sensitive to surface level properties, but in a matching game context, can they be sensitive to higher-order properties too?",
-//   research_rights_statement:
-//     "You are not waiving any legal claims, rights or remedies because of your participation in this research study. If you feel you have been treated unfairly, or you have questions regarding your rights as a research subject, you may contact the Committee On the Use of Humans as Experimental Subjects (COUHES), Massachusetts of Technology, 77 Massachusetts Avenue Cambridge, MA 02139 (contact: couhes@mit.edu)",
-// };
-// timeline.push(videoConsent);
+const videoConsent = {
+  type: chsRecord.VideoConsentPlugin,
+  PIName: "Laura Schulz",
+  institution: "Massachussetts Institute of Technology",
+  PIContact: "Laura Schulz (contact: nhcoates@mit.edu)",
+  payment:
+    "You can only participate in this study once for compensation. After you finish the study, we will email you a $5 Amazon gift card from Amazon.com (via a code that can be entered online) within ten days to thank you for your time. To be eligible for the gift card, (1) your child must be in the age range for this study, (2) English is (one of) your child's first language(s), (3) need to submit a valid consent statement in which we can see you AND your child in the frame, (4) must not have participated in similar matching tasks (you will get a warning message if this is the case!). Please make sure you are with your child throughout, especially if they cannot click the buttons (they can point and you can click their answer).",
+  procedures:
+    "In this study, you and your child will see eight matching tasks that involve boxes with shapes. Each time, you and your child will listen to the instructions, and pick which box matches best with the target box. Throughout the experiment, you or your child will click to make responses (if your child cannot click, they can point to the answer and you can click the answer they choose).",
+  purpose:
+    "This study aims to explore how very young children, age 4 to 5-years-old, come to understand relational mappings. This is worth exploring because many studies suggest that young children are more sensitive to surface level properties, but in a matching game context, can they be sensitive to higher-order properties too?",
+  research_rights_statement:
+    "You are not waiving any legal claims, rights or remedies because of your participation in this research study. If you feel you have been treated unfairly, or you have questions regarding your rights as a research subject, you may contact the Committee On the Use of Humans as Experimental Subjects (COUHES), Massachusetts of Technology, 77 Massachusetts Avenue Cambridge, MA 02139 (contact: couhes@mit.edu)",
+};
+timeline.push(videoConsent);
 
 const instructions = [
   {
@@ -223,7 +223,7 @@ const more = [
 
 let condA = [];
 let condB = [];
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 8; i++) {
   condA = condA.concat(trialsA[i]);
   condB = condB.concat(trialsB[i]);
   condA.push(more[i]);
@@ -245,8 +245,8 @@ console.log(condition_assignment);  // TODO its here for debug purposes
 const site = '<a href="https://eccl.mit.edu">https://www.eccl.mit.edu</a>';
 const debrief_string = `We showed your child a sample box with three shapes. Children were asked to pick which of two other boxes of shapes was the best match for the first box. One box was a distractor with an irrelevant relationship among the shapes; the other box (the matching box) had a relationship among the shapes similar to the relationship in the first box (e.g. the three shapes progressively got bigger or smaller, or the relationship was symmetric with two large shapes flanking a small shape or vice versa). <br><br>
 Across trials, children saw both 'easy' matches (the relationships between the sample and the match were identical) and 'hard' matches (the relationship in the match was the inverse of the relationship in the sample-- for instance if the shapes in the sample box had gone from smallest on the left to largest on the right, the shapes in the box went from largest on the left to smallest on the right). Previous, in-person research had suggested that although young children could succeed on easy matches, they struggled with hard matches. Here we aimed to replicate the result in an online study.<br><br>
-This is the first study in a planned series of studies on children's understanding of relational matches. We think that three-year-olds may struggle with this kind of task because children are looking not only at the relationship between the shapes in the boxes but also at the relationship among the boxes themselves. In the current set up, there is a matching box and a distractor box: thus, one of the boxes has no relationship at all with the others. We believe that young children will have an easier time recognizing relational matches in a context where every box has a match (i.e., there are no distractors). That is, we believe that if we show children two sample boxes (each with a different relationship among the shapes) and two kinds of matching boxes (each matching to one of the relationships), children will be more likely to recognize the contrasting relationships and succeed at the task. However, we needed to replicate the failure first before going on to test the conditions that might lead children to succeed.<br>
-<b>Why is this important?</b><br><br>
+This is the first study in a planned series of studies on children's understanding of relational matches. We think that three-year-olds may struggle with this kind of task because children are looking not only at the relationship between the shapes in the boxes but also at the relationship among the boxes themselves. In the current set up, there is a matching box and a distractor box: thus, one of the boxes has no relationship at all with the others. We believe that young children will have an easier time recognizing relational matches in a context where every box has a match (i.e., there are no distractors). That is, we believe that if we show children two sample boxes (each with a different relationship among the shapes) and two kinds of matching boxes (each matching to one of the relationships), children will be more likely to recognize the contrasting relationships and succeed at the task. However, we needed to replicate the failure first before going on to test the conditions that might lead children to succeed.<br><br>
+<b>Why is this important?</b><br>
 This study is important for three reasons. First, replicating children's failure to infer abstract, non-identical relationships will show both that the original finding is robust and that we can replicate in-person laboratory experiments online. Second, this will allow us to build on the initial study, potentially showing that even young children can in fact infer abstract, non-identical, higher-order relationships when they are not faced with irrelevant distractors. This would suggest the importance of how we teach and present information to young children. Showing children one matching pair and one distractor may make it difficult for children to know what kind of relationship they should attend to; by contrast, showing children two different matching relationships may serve to highlight the contrast and allow children to more readily detect the alignment between the samples and the matches. Overall, children may do better in 'errorless' contexts where 'everything has an answer' than in contexts where one answer is right and one is wrong.<br><br>
 If you want to learn more about studies similar to this one, please visit our lab website at ${site}<br>
 <b><center>The experiment is complete. Thank you for participating! You will receive an email from Lookit with your payment information. Please close this window.</center></b>`;
